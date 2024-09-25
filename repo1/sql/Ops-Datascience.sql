15133c470247c66d04df44fcd9de15823b4a5ad5
Joseph Nemuri <c_Joseph.Nemuri@viasat.com>
2024-09-25 19:18:49 -0400

    test

--- repo1/sql/Ops-Datascience.sql
+++
@@ -1,4 +0,0 @@
-select * from [ops-datascience-5026].[WIP Location Summary (MH)];
-
-select * from [ops-datascience-5026].[Upcoming WIP Summary (MH)]
-select * from [ops-datascience-5026].[WIP Location Summary (MH)]
